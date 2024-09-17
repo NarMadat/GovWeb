56ebbe66cb8017dc6680b5bf16c68804ed538708
Narek <137328861+NarMadat@users.noreply.github.com>
2024-09-17 18:01:26 +0400

    Update DonePage.vue

--- src/views/DonePage.vue
+++ src/views/DonePage.vue
@@ -1,27 +1,27 @@
 <template>
     <div class="px-4 md:px-8 lg:ml-40 lg:mr-40 py-4 mt-10">
         <div class="h-60 done flex">
-            <h2 class="text-white text-5xl md:text-5xl px-4 py-16">Done</h2>
+            <h2 class="text-white text-5xl md:text-5xl px-4 py-16">Your site is ready</h2>
         </div>
 
         <div class="mt-12">
-            <h2 class="text-2xl font-medium">Information:</h2>
+            <h2 class="text-2xl font-medium">Your next steps:</h2>
 
             <div class="wysiwyg regular mt-4">
                 <ul>
                     <li dir="ltr" aria-level="1" class="flex items-center">
                         <p dir="ltr" role="presentation">
-                            Text
+                            the website URL will be sent to you via email
                         </p>
                     </li>
                     <li dir="ltr" aria-level="1" class="flex items-center mt-2">
                         <p dir="ltr" role="presentation">
-                            Text
+                            remember to create administrator users
                         </p>
                     </li>
                     <li dir="ltr" aria-level="1" class="flex items-center mt-2">
                         <p dir="ltr" role="presentation">
-                            Text
+                            you will need to set up your web domain.
                         </p>
                     </li>
                 </ul>
